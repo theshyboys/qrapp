f3048d2bc31e8c5e94db728b64ec2a9b874f06de
TEERA YOOSUK <theshyboys@gmail.com>
2025-04-16 14:09:06 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,26 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/theshyboys/qrapp.git
 git push -u origin main
+
+
+
+https://shopping-two-rosy.vercel.app/
+echo "# shopping" >> README
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
+
+
+https://vercel.com/theshyboys-projects
+https://vercel.com/new?teamSlug=theshyboys-projects
+
+
+https://th.qr-code-generator.com/
+
+
 */
 
 
